fd75644307bddb2bac7dce0ae67add3f549d4d47
jbphet <john.blanco@colorado.edu>
2013-10-17 09:32:32 -0700

    Added icons - temporary versions - for the home screen and nav bar.

--- js/balancing-act-main.js
+++ js/balancing-act-main.js
@@ -12,11 +12,15 @@ require( [
   'BALANCING_ACT/BalancingActTempModel',
   'BALANCING_ACT/BalancingActTempView',
   'JOIST/SimLauncher',
+  'SCENERY/nodes/Image',
   'string!BALANCING_ACT/balancing-act.name',
   'string!BALANCING_ACT/intro',
   'string!BALANCING_ACT/balanceLab',
-  'string!BALANCING_ACT/game'
-], function( Text, Rectangle, Sim, BalancingActTempModel, BalancingActTempView, SimLauncher, simTitleString, introString, balanceLabString, gameString  ) {
+  'string!BALANCING_ACT/game',
+  'image!BALANCING_ACT/first-tab-icon.png',
+  'image!BALANCING_ACT/second-tab-icon.png',
+  'image!BALANCING_ACT/third-tab-icon.png'
+], function( Text, Rectangle, Sim, BalancingActTempModel, BalancingActTempView, SimLauncher, Image, simTitleString, introString, balanceLabString, gameString, firstTabIcon, secondTabIcon, thirdTabIcon  ) {
   'use strict';
 
   SimLauncher.launch( function() {
@@ -29,21 +33,21 @@ require( [
     new Sim( simTitleString, [
       {
         name: introString,
-        icon: new Rectangle( 0, 0, 50, 50, {fill: 'blue'} ),
+        icon: new Image( firstTabIcon ),
         createModel: function() {return new BalancingActTempModel( 768, 504 );},
         createView: function( model ) {return new BalancingActTempView( model );},
         backgroundColor: "#9ddcf8"
       },
       {
         name: balanceLabString,
-        icon: new Rectangle( 0, 0, 50, 50, {fill: 'blue'} ),
+        icon: new Image( secondTabIcon ),
         createModel: function() {return new BalancingActTempModel( 768, 504 );},
         createView: function( model ) {return new BalancingActTempView( model );},
         backgroundColor: "#9ddcf8"
       },
       {
         name: gameString,
-        icon: new Rectangle( 0, 0, 50, 50, {fill: 'blue'} ),
+        icon: new Image( thirdTabIcon ),
         createModel: function() {return new BalancingActTempModel( 768, 504 );},
         createView: function( model ) {return new BalancingActTempView( model );},
         backgroundColor: "#9ddcf8"
