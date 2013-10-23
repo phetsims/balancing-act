f9a9433994a4f5d6fff61d3b28271707513e5e2c
jbphet <john.blanco@colorado.edu>
2013-10-23 16:54:14 -0700

    Added the OutsideBackgroundNode and the nodes upon which it depends.  These may well be moved to common code at some point.

---
+++ js/common/view/OutsideBackgroundNode.js
@@ -0,0 +1,51 @@
+// Copyright 2002-2013, University of Colorado Boulder
+
+/**
+ * This node is intended for use as a background on a screen, and shows the
+ * ground on the bottom and the sky on the top.
+ * <p/>
+ * This assumes that the horizon is at Y=0.
+ *
+ * @author John Blanco
+ * @author Sam Reid
+ */
+define( function( require ) {
+  'use strict';
+
+  // Imports
+  var Rectangle = require( 'DOT/Rectangle' );
+  var SkyNode = require( 'BALANCING_ACT/common/view/SkyNode' );
+  var GroundNode = require( 'BALANCING_ACT/common/view/GroundNode' );
+  var inherit = require( 'PHET_CORE/inherit' );
+  var Node = require( 'SCENERY/nodes/Node' );
+
+  /**
+   * @param {ModelViewTransform2} mvt Model view transform
+   * @param {number} skyGradientTopY Top of the gradient in model coordinates
+   * @param {number} groundGradientBottomY Bottom of the gradient in model coordinates
+   * @param {Object} options
+   * @constructor
+   */
+  function OutsideBackgroundNode( mvt, skyGradientTopY, groundGradientBottomY, options ) {
+
+    Node.call( this );
+
+    options = _.extend(
+      {
+        // Defaults.
+        modelRect: new Rectangle( -1000, -2000, 2000, 4000 )
+      }
+      , options );
+
+    if ( options.modelRect.getMinY() < 0 ) {
+      // Add the ground first, because we're earthy people.
+      this.addChild( new GroundNode( mvt, options.modelRect, groundGradientBottomY ) );
+    }
+    if ( options.modelRect.getMaxY() > 0 ) {
+      // Add the sky.
+      this.addChild( new SkyNode( mvt, options.modelRect, skyGradientTopY ) );
+    }
+  }
+
+  return inherit( Node, OutsideBackgroundNode );
+} );
