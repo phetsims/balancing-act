e0c5a8e5c336dfa6ae4c3c996de0553ac0da0ad8
jbphet <john.blanco@colorado.edu>
2013-11-29 17:12:34 -0800

    Initial port of the 'ShapeMass' type.

--- js/common/model/ShapeMass.js
+++ js/common/model/ShapeMass.js
@@ -1,3 +1,32 @@
 // Copyright 2002-2013, University of Colorado Boulder
 
-// TODO: Implement, stubbed in order to be able to deploy intermediate versions.
+/**
+ * This type defines a mass in the model whose appearance is defined by its
+ * shape as opposed to, say, an image.
+ *
+ * @author John Blanco
+ */
+define( function( require ) {
+  'use strict';
+
+  // Imports
+  var inherit = require( 'PHET_CORE/inherit' );
+  var Mass = require( 'BALANCING_ACT/common/model/Mass' );
+  var Property = require( 'Mass' );
+  var Vector2 = require( 'DOT/Vector2' );
+
+  function ShapeMass( massValue, shape ) {
+    Mass.call( this, massValue );
+
+    // TODO: Is this really needed, i.e. do any of the sub-types change shape
+    // once constructed?  If not, it may make sense to do away with this level
+    // of the inheritance hierarchy altogether.
+    this.shapeProperty = new Property( shape );
+  }
+
+  return inherit( Mass, ShapeMass, {
+    getMiddlePoint: function() {
+      return this.shapeProperty.value.bounds.center.rotated( this.rotationAngle ).plus( this.position );
+    }
+  } );
+} );
