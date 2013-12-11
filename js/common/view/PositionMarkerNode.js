d1508f38b22f3a9e26e245db1d50dee57ab5921f
jbphet <john.blanco@colorado.edu>
2013-12-11 14:04:17 -0800

    Improved the appearance of the position markers based on feedback from @ariel-phet.

--- js/common/view/PositionMarkerNode.js
+++ js/common/view/PositionMarkerNode.js
@@ -16,16 +16,16 @@ define( function( require ) {
 
   // Constants
   var color = 'rgb( 255, 153, 0 )';
-  var LINE_LENGTH = 15; // empirically chosen
+  var LINE_LENGTH = 14; // empirically chosen
   var CIRCLE_RADIUS = 3; // empirically chosen
 
   function PositionMarkerNode( labelText, options ) {
     Node.call( this );
-    var line = new Line( 0, 0, 0, LINE_LENGTH, { stroke: color, lineWidth: 2, lineDash: [ 2, 3 ] } );
+    var line = new Line( 0, 0, 0, LINE_LENGTH, { stroke: color, lineWidth: 2, lineDash: [ 2, 2 ] } );
     this.addChild( line );
     var circle = new Circle( CIRCLE_RADIUS, { fill: color, centerX: 0, centerY: LINE_LENGTH} );
     this.addChild( circle );
-    this.addChild( new Text( labelText, { font: new PhetFont( {size: 12, weight: 'bold'} ), centerX: 0, top: circle.bottom + 1 } ) );
+    this.addChild( new Text( labelText, { font: new PhetFont( {size: 12, weight: 'bold'} ), centerX: 0, top: circle.bottom - 2 } ) );
     this.mutate( options );
   }
 
