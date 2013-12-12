9a9042964cd410237a6bba41efa82cfc70b463de
jbphet <john.blanco@colorado.edu>
2013-12-12 08:56:53 -0800

    Started adding the full set of bricks to the mass carousel in tab 2.

--- js/common/view/MassKitSelectionNode.js
+++ js/common/view/MassKitSelectionNode.js
@@ -13,12 +13,15 @@ define( function( require ) {
   var BoyCreatorNode = require( 'BALANCING_ACT/balancelab/view/BoyCreatorNode' );
   var BrickStackCreatorNode = require( 'BALANCING_ACT/balancelab/view/BrickStackCreatorNode' );
   var bricksString = require( 'string!BALANCING_ACT/bricks' );
+  var HBox = require( 'SCENERY/nodes/HBox' );
   var inherit = require( 'PHET_CORE/inherit' );
   var KitSelectionNode = require( 'BALANCING_ACT/common/view/KitSelectionNode' );
+  var Node = require( 'SCENERY/nodes/Node' );
   var peopleString = require( 'string!BALANCING_ACT/people' );
   var PhetFont = require( 'SCENERY_PHET/PhetFont' );
   var Property = require( 'AXON/Property' );
   var Text = require( 'SCENERY/nodes/Text' );
+  var VBox = require( 'SCENERY/nodes/VBox' );
   var WomanCreatorNode = require( 'BALANCING_ACT/balancelab/view/WomanCreatorNode' );
 
   // TODO: Temp for testing --------------
@@ -29,6 +32,27 @@ define( function( require ) {
   var TITLE_FONT = new PhetFont( 16 );
 
   function MassKitSelectionNode( model, mvt, options ) {
+
+    // Lay out the kit node for creating bricks of various sizes.
+    var brickCreatorKit = new Node();
+    brickCreatorKit.addChild( new VBox(
+      {
+        children: [
+          new HBox(
+            {
+              children: [ new BrickStackCreatorNode( 1, model, mvt ), new BrickStackCreatorNode( 2, model, mvt ) ],
+              spacing: 20
+
+            } ),
+          new HBox(
+            {
+              children: [ new BrickStackCreatorNode( 3, model, mvt ), new BrickStackCreatorNode( 4, model, mvt ) ],
+              spacing: 20
+            } )
+        ],
+        spacing: 5
+      } ) );
+
     this.selectedKit = new Property( 0 );
     KitSelectionNode.call( this, this.selectedKit,
       [
