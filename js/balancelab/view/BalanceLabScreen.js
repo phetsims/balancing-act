c8295baf88f9d7ee0bbc1b6e72cbd682b83c95cb
jbphet <john.blanco@colorado.edu>
2013-12-11 20:32:51 -0800

    Positioned mass kit in roughly correct place for demo.

--- js/balancelab/view/BalanceLabScreen.js
+++ js/balancelab/view/BalanceLabScreen.js
@@ -10,12 +10,9 @@ define( function( require ) {
   var inherit = require( 'PHET_CORE/inherit' );
   var BasicBalanceScreen = require( 'BALANCING_ACT/common/view/BasicBalanceScreen' );
   var MassKitSelectionNode = require( 'BALANCING_ACT/common/view/MassKitSelectionNode' );
+  var Panel = require( 'SUN/Panel' );
   var Property = require( 'AXON/Property' );
   var Vector2 = require( 'DOT/Vector2' );
-
-  // TODO: temp for testing.
-  var BrickStackCreatorNode = require( 'BALANCING_ACT/balancelab/view/BrickStackCreatorNode' ); // TODO: For testing, remove when not needed.
-  var BoyCreatorNode = require( 'BALANCING_ACT/balancelab/view/BoyCreatorNode' ); // TODO: For testing, remove when not needed.
 
   /**
    * @param model
@@ -25,16 +22,15 @@ define( function( require ) {
     BasicBalanceScreen.call( this, model );
     var thisScreen = this;
 
-    //TODO for testing only, remove.
-    this.addChild( new BrickStackCreatorNode( 2, model, thisScreen.mvt ).mutate( { centerX: 650, bottom: 400 } ) );
-    this.addChild( new BoyCreatorNode( model, thisScreen.mvt ).mutate( { centerX: 700, bottom: 400 } ) );
-
     var KitControlNode = require( 'BALANCING_ACT/common/view/KitControlNode' );
     var Property = require( 'AXON/Property' );
     var Text = require( 'SCENERY/nodes/Text' );
 
     // TODO: May want different MVT so that nodes are scaled a bit differently.
-    this.addChild( new MassKitSelectionNode( model, thisScreen.mvt, { left: 50 } ) );
+    var massKitSelectionNode = new MassKitSelectionNode( model, thisScreen.mvt );
+    massKitSelectionNode.centerX = 740;
+    massKitSelectionNode.bottom = 450;
+    this.addChild( massKitSelectionNode );
   }
 
   return inherit( BasicBalanceScreen, BalanceLabScreen );
