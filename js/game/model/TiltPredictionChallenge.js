b18331d887d8a64b0f040f552011ec33bfc3ac8e
jbphet <john.blanco@colorado.edu>
2014-01-20 15:41:53 -0800

    Initial port of Tilt Prediction challenge.

---
+++ js/game/model/TiltPredictionChallenge.js
@@ -0,0 +1,46 @@
+// Copyright 2002-2014, University of Colorado Boulder
+
+/**
+ * A challenge, used in the balance game, in which the user must predict which
+ * way the plank will tilt when the supports are removed.
+ *
+ * @author John Blanco
+ */
+define( function( require ) {
+  'use strict';
+
+  // Imports
+  var inherit = require( 'PHET_CORE/inherit' );
+  var BalanceGameChallenge = require( 'BALANCING_ACT/game/model/BalanceGameChallenge' );
+  var whatWillHappenString = require( 'string!BALANCING_ACT/whatWillHappen' );
+
+  /**
+   * @constructor
+   */
+  function TiltPredictionChallenge( fixedMasses ) {
+    BalanceGameChallenge.call( this, 'doubleColumns' );
+    Array.prototype.push.apply( this.fixedMassDistancePairs, fixedMasses );
+
+    // Set up the challenge view configuration, which provides information to
+    // the view about how this challenge should be displayed.
+    this.viewConfig = {
+      title: whatWillHappenString,
+      showMassEntryDialog: false,
+      showTiltPredictionSelector: true
+    }
+  }
+
+  return inherit( BalanceGameChallenge, TiltPredictionChallenge, {
+    // Convenience method for creating this type of challenge.
+    create: function( fixedMass1, fixedMass1DistanceFromCenter, fixedMass2, fixedMass2DistanceFromCenter ) {
+
+      // Add the fixed masses and their distances from the center of the balance.
+      var fixedMassesList = [];
+      fixedMassesList.push( { mass: fixedMass1, distance: fixedMass1DistanceFromCenter } );
+      fixedMassesList.push( { mass: fixedMass2, distance: fixedMass2DistanceFromCenter } );
+
+      // Create the actual challenge.
+      return new TiltPredictionChallenge( fixedMassesList );
+    }
+  } );
+} );
