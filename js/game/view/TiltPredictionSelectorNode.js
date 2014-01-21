4a108b2a9556e4bf86769fbfb50b7b7b46139496
jbphet <john.blanco@colorado.edu>
2014-01-20 16:06:24 -0800

    Initial port of the tilt prediction selector node.

--- js/game/view/TiltPredictionSelectorNode.js
+++ js/game/view/TiltPredictionSelectorNode.js
@@ -1,23 +1,43 @@
 // Copyright 2002-2014, University of Colorado Boulder
 
+/**
+ * A Scenery node that defines a user interface element which allows the user
+ * to select one of three possible ways in which the balance might behave -
+ * tilt left, tilt right, or stay balanced.
+ *
+ * @author John Blanco
+ */
 define( function( require ) {
   'use strict';
 
   // Imports
+  var HBox = require( 'SCENERY/nodes/HBox' );
   var inherit = require( 'PHET_CORE/inherit' );
   var Node = require( 'SCENERY/nodes/Node' );
+  var Panel = require( 'SUN/Panel' );
+  var plankBalancedImage = require( 'image!BALANCING_ACT/plank-balanced.png' );
+  var plankTippedLeftImage = require( 'image!BALANCING_ACT/plank-tipped-left.png' );
+  var plankTippedRightImage = require( 'image!BALANCING_ACT/plank-tipped-left.png' );
   var Property = require( 'AXON/Property' );
+  var TiltPredictionSelectionPanel = require( 'BALANCING_ACT/game/view/TiltPredictionSelectionPanel' );
+  var VBox = require( 'SCENERY/nodes/VBox' );
 
-  function TiltPredictionSelectorNode() {
+  function TiltPredictionSelectorNode( gameStateProperty ) {
     Node.call( this );
-    // Property that tracks the selected prediction.  Value values are 'none',
+
+    // Property that tracks the selected prediction.  Valid values are 'none',
     // 'tiltDownOnLeftSide', 'stayBalanced', and 'tiltDownOnRightSide'.
     this.tiltPredictionProperty = new Property( 'none' );
 
-    //TODO: Implement the actual selection node.
+    var panelContents = new HBox(
+      { children: [
+        new TiltPredictionSelectionPanel( plankTippedLeftImage, 'tiltDownOnLeftSide', this.tiltPredictionProperty, gameStateProperty ),
+        new TiltPredictionSelectionPanel( plankBalancedImage, 'stayBalanced', this.tiltPredictionProperty, gameStateProperty ),
+        new TiltPredictionSelectionPanel( plankTippedRightImage, 'tiltDownOnRightSide', this.tiltPredictionProperty, gameStateProperty )
+      ] } );
+
+    this.addChild( new Panel( panelContents ) );
   }
 
-  return inherit( Node, TiltPredictionSelectorNode, {
-    //TODO prototypes
-  } );
+  return inherit( Node, TiltPredictionSelectorNode );
 } );
