4836e44b386898b9e8f53dabf5449d0dabd3621b
jbphet <john.blanco@colorado.edu>
2014-01-21 17:33:03 -0800

    Added hover indication to the tilt prediction selection panel.

--- js/game/view/TiltPredictionSelectionPanel.js
+++ js/game/view/TiltPredictionSelectionPanel.js
@@ -18,10 +18,11 @@ define( function( require ) {
   var PANEL_WIDTH = 170; // In screen coords, fairly close to pixels.  Empirically determined.
   var NON_HIGHLIGHT_COLOR = 'black';
   var NON_HIGHLIGHT_LINE_WIDTH = 1;
+  var HOVER_LINE_WIDTH = 2;
   var SELECTED_HIGHLIGHT_COLOR = 'rgb( 255, 215, 0 )';
+  var MOUSE_OVER_HIGHLIGHT_COLOR = 'rgb( 255, 215, 0 )';
   var SELECTED_HIGHLIGHT_LINE_WIDTH = 6;
   var CORRECT_ANSWER_HIGHLIGHT_COLOR = 'rgb( 0, 255, 0 )';
-  var CORRECT_ANSWER_HIGHLIGHT_LINE_WIDTH = 6;
   var INVISIBLE_COLOR = 'rgba( 0, 0, 0, 0 )';
 
   /**
@@ -30,22 +31,55 @@ define( function( require ) {
   function TiltPredictionSelectionPanel( image, correspondingPrediction, tiltPredictionProperty, gameStateProperty ) {
     Node.call( this );
     var thisNode = this;
+
+    // Add the image.
     var imagePanel = new Image( image, { cursor: 'pointer' } );
     imagePanel.scale( PANEL_WIDTH / imagePanel.width );
     thisNode.addChild( imagePanel );
 
-    // Set up mouse listener that watches to see if the user has
-    // selected this option.
-    imagePanel.addInputListener( new DownUpListener(
-      {
-        up: function( event ) {
-          tiltPredictionProperty.value = correspondingPrediction;
+    // Define a function for updating the highlight state
+    function updateHighlightState() {
+      if ( tiltPredictionProperty.value === correspondingPrediction ) {
+        thisNode.thinOutline.stroke = INVISIBLE_COLOR;
+        if ( gameStateProperty.value === 'displayingCorrectAnswer' ) {
+          thisNode.thickOutline.stroke = CORRECT_ANSWER_HIGHLIGHT_COLOR;
         }
-      } ) );
+        else {
+          thisNode.thickOutline.stroke = SELECTED_HIGHLIGHT_COLOR;
+        }
+      }
+      else {
+        thisNode.thickOutline.stroke = INVISIBLE_COLOR;
+        thisNode.thinOutline.stroke = NON_HIGHLIGHT_COLOR;
+        if ( thisNode.mouseOver ) {
+          thisNode.thinOutline.lineWidth = HOVER_LINE_WIDTH;
+        }
+        else {
+          thisNode.thinOutline.lineWidth = NON_HIGHLIGHT_LINE_WIDTH;
+        }
+      }
+    }
+
+    // Set up mouse listener that watches to see if the user has selected this option.
+    imagePanel.addInputListener( new DownUpListener( { up: function( event ) { tiltPredictionProperty.value = correspondingPrediction; } } ) );
+
+    // Set up a hover listener to update hover highlight.
+    thisNode.mouseOver = false;
+    imagePanel.addInputListener(
+      {
+        over: function() {
+          thisNode.mouseOver = true;
+          updateHighlightState();
+        },
+        out: function() {
+          thisNode.mouseOver = false;
+          updateHighlightState();
+        }
+      } );
 
     // Add the outline around the panel, which will be changed to depict the
     // user's selection and the correct answer.
-    var outlineShape = Shape.rect( imagePanel.bounds.minX, imagePanel.bounds.minY, imagePanel.bounds.width, imagePanel.bounds.minX )
+    var outlineShape = Shape.rect( imagePanel.bounds.minX, imagePanel.bounds.minY, imagePanel.bounds.width, imagePanel.bounds.minX );
     thisNode.thinOutline = new Rectangle.bounds( imagePanel.bounds,
       {
         stroke: NON_HIGHLIGHT_COLOR,
@@ -62,30 +96,14 @@ define( function( require ) {
     // Add listener for changes to the tilt prediction.
     tiltPredictionProperty.link( function( predictionValue ) {
       // Turn the highlight on or off.
-      thisNode.updateHighlightState( predictionValue === correspondingPrediction, gameStateProperty.value === 'displayingCorrectAnswer' );
+      updateHighlightState();
     } );
 
     // Add listener for changes to the game state.
     gameStateProperty.link( function( gameState ) {
-      thisNode.updateHighlightState( tiltPredictionProperty.value === correspondingPrediction, gameState === 'displayingCorrectAnswer' );
+      updateHighlightState();
     } );
   }
 
-  return inherit( Node, TiltPredictionSelectionPanel, {
-    updateHighlightState: function( selectionMatches, displayingCorrectAnswer ) {
-      if ( selectionMatches ) {
-        this.thinOutline.stroke = INVISIBLE_COLOR;
-        if ( displayingCorrectAnswer ) {
-          this.thickOutline.stroke = CORRECT_ANSWER_HIGHLIGHT_COLOR;
-        }
-        else {
-          this.thickOutline.stroke = SELECTED_HIGHLIGHT_COLOR;
-        }
-      }
-      else {
-        this.thinOutline.stroke = NON_HIGHLIGHT_COLOR;
-        this.thickOutline.stroke = INVISIBLE_COLOR;
-      }
-    }
-  } );
+  return inherit( Node, TiltPredictionSelectionPanel );
 } );
