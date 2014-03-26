22b59ec4b7de310e3829a2876343d6e59c52c005
jbphet <john.blanco@colorado.edu>
2014-03-26 10:26:41 -0700

    Widened tolerance of image scaling for images before hitting assertion, updated changes.txt.

--- js/common/view/ImageMassNode.js
+++ js/common/view/ImageMassNode.js
@@ -61,7 +61,7 @@ define( function( require ) {
       var scalingFactor = Math.abs( mvt.modelToViewDeltaY( imageMass.height ) ) / imageNode.height;
       // Debug printouts for indicating whether the image for this node is
       // reasonably scaled.  Generally, scale values between 0.5 and 1 look best.
-      assert && assert( scalingFactor >= 0.5 && scalingFactor <= 2, 'Warning: Scaling factor is too large or small, drawing size should be adjusted. Image src = ' + image.getAttribute( 'src' ) + ', scaling factor = ' + scalingFactor );
+      assert && assert( scalingFactor >= 0.4 && scalingFactor <= 2, 'Warning: Scaling factor is too large or small, drawing size should be adjusted. Image src = ' + image.getAttribute( 'src' ) + ', scaling factor = ' + scalingFactor );
       assert && assert( scalingFactor <= 1.0, 'Warning: Scaling image up, could lead to blurriness, scaling factor = ' + scalingFactor );
       imageNode.scale( scalingFactor );
       imageNode.centerX = 0;
