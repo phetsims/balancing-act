5866c030e70935fa7d2536c420fd57d45f3d2bca
zepumph <michael.kauzmann@colorado.edu>
2019-07-11 12:54:12 -0800

    update copyright dates, https://github.com/phetsims/tasks/issues/988

--- js/common/model/Mass.js
+++ js/common/model/Mass.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2017, University of Colorado Boulder
+// Copyright 2013-2019, University of Colorado Boulder
 
 /**
  * Base class for all objects that can be placed on the balance.
--- js/common/view/PositionedVectorNode.js
+++ js/common/view/PositionedVectorNode.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2017, University of Colorado Boulder
+// Copyright 2013-2019, University of Colorado Boulder
 
 /**
  * Class that depicts a vector that has an origin as well as x and y components,
