da93ee9404685f6347a4868849267a0c28cea4e9
zepumph <michael.kauzmann@colorado.edu>
2020-01-27 16:58:15 -0900

    add phetioDocumentation, https://github.com/phetsims/joist/issues/591

--- js/phet-io/balancing-act-phet-io-elements-baseline.js
+++ js/phet-io/balancing-act-phet-io-elements-baseline.js
@@ -5618,7 +5618,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioTypeName": "PropertyIO<BooleanIO>"
     },
     "balancingAct.general.soundManager.enabledProperty": {
-      "phetioDocumentation": "",
+      "phetioDocumentation": "If this sim contains sound, then this Property will toggle whether sound is enabled ordisabled.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
       "phetioFeatured": false,
