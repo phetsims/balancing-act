4d2f5a4ef70a7f9424967e021e3cbca2f8139bfe
phet-dev <phetadmin@gmail.com>
2020-01-30 03:08:08 -0700

    update copyright dates from daily grunt work

--- js/common/model/masses/LabeledImageMass.js
+++ js/common/model/masses/LabeledImageMass.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2019, University of Colorado Boulder
+// Copyright 2013-2020, University of Colorado Boulder
 
 /**
  * This is an extension of the ImageMass type that adds a textual label.  This
