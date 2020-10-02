627c0e73d0583b1a9f4ae6f03cd084d90d54c087
pixelzoom <cmalley@pixelzoom.com>
2020-10-01 21:42:08 -0600

    convert Panel subclasses to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/common/view/PositionIndicatorControlPanel.js
+++ js/common/view/PositionIndicatorControlPanel.js
@@ -7,7 +7,6 @@
  * @author John Blanco
  */
 
-import inherit from '../../../../phet-core/js/inherit.js';
 import merge from '../../../../phet-core/js/merge.js';
 import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
 import HBox from '../../../../scenery/js/nodes/HBox.js';
@@ -18,8 +17,8 @@ import VStrut from '../../../../scenery/js/nodes/VStrut.js';
 import Panel from '../../../../sun/js/Panel.js';
 import VerticalAquaRadioButtonGroup from '../../../../sun/js/VerticalAquaRadioButtonGroup.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
-import balancingActStrings from '../../balancingActStrings.js';
 import balancingAct from '../../balancingAct.js';
+import balancingActStrings from '../../balancingActStrings.js';
 import PositionIndicatorChoice from '../model/PositionIndicatorChoice.js';
 
 const marksString = balancingActStrings.marks;
@@ -31,56 +30,56 @@ const rulersString = balancingActStrings.rulers;
 const RADIO_BUTTON_TEXT_OPTIONS = { font: new PhetFont( 14 ) };
 const TITLE_TEXT_OPTIONS = { font: new PhetFont( 16 ) };
 
-/**
- * @param {Property} positionIndicatorStateProperty
- * @param {Object} [options]
- * @constructor
- */
-function PositionIndicatorControlPanel( positionIndicatorStateProperty, options ) {
+class PositionIndicatorControlPanel extends Panel {
 
-  options = merge( {
-    titleToControlsVerticalSpace: 5,
-    fill: 'rgb( 240, 240, 240 )',
-    xMargin: 5,
-    align: 'left',
-    tandem: Tandem.REQUIRED
-  }, options );
+  /**
+   * @param {Property} positionIndicatorStateProperty
+   * @param {Object} [options]
+   */
+  constructor( positionIndicatorStateProperty, options ) {
 
-  const positionRadioButtonGroup = new VerticalAquaRadioButtonGroup( positionIndicatorStateProperty, [
-    {
-      node: new Text( noneString, RADIO_BUTTON_TEXT_OPTIONS ),
-      value: PositionIndicatorChoice.NONE,
-      tandemName: 'noneRadioButton'
-    },
-    {
-      node: new Text( rulersString, RADIO_BUTTON_TEXT_OPTIONS ),
-      value: PositionIndicatorChoice.RULERS,
-      tandemName: 'rulerRadioButton'
-    },
-    {
-      node: new Text( marksString, RADIO_BUTTON_TEXT_OPTIONS ),
-      value: PositionIndicatorChoice.MARKS,
-      tandemName: 'marksRadioButton'
-    }
-  ], {
-    radioButtonOptions: { radius: 6 },
-    touchAreaDilation: 5,
-    tandem: options.tandem.createTandem( 'positionRadioButtonGroup' )
-  } );
+    options = merge( {
+      titleToControlsVerticalSpace: 5,
+      fill: 'rgb( 240, 240, 240 )',
+      xMargin: 5,
+      align: 'left',
+      tandem: Tandem.REQUIRED
+    }, options );
 
-  const positionMarkerVBox = new VBox( {
-    children: [
-      new Text( positionString, TITLE_TEXT_OPTIONS ),
-      new VStrut( options.titleToControlsVerticalSpace ),
-      new HBox( { children: [ new HStrut( 10 ), positionRadioButtonGroup ] } )
-    ],
-    align: 'left'
-  } );
+    const positionRadioButtonGroup = new VerticalAquaRadioButtonGroup( positionIndicatorStateProperty, [
+      {
+        node: new Text( noneString, RADIO_BUTTON_TEXT_OPTIONS ),
+        value: PositionIndicatorChoice.NONE,
+        tandemName: 'noneRadioButton'
+      },
+      {
+        node: new Text( rulersString, RADIO_BUTTON_TEXT_OPTIONS ),
+        value: PositionIndicatorChoice.RULERS,
+        tandemName: 'rulerRadioButton'
+      },
+      {
+        node: new Text( marksString, RADIO_BUTTON_TEXT_OPTIONS ),
+        value: PositionIndicatorChoice.MARKS,
+        tandemName: 'marksRadioButton'
+      }
+    ], {
+      radioButtonOptions: { radius: 6 },
+      touchAreaDilation: 5,
+      tandem: options.tandem.createTandem( 'positionRadioButtonGroup' )
+    } );
 
-  Panel.call( this, positionMarkerVBox, options );
+    const positionMarkerVBox = new VBox( {
+      children: [
+        new Text( positionString, TITLE_TEXT_OPTIONS ),
+        new VStrut( options.titleToControlsVerticalSpace ),
+        new HBox( { children: [ new HStrut( 10 ), positionRadioButtonGroup ] } )
+      ],
+      align: 'left'
+    } );
+
+    super( positionMarkerVBox, options );
+  }
 }
 
 balancingAct.register( 'PositionIndicatorControlPanel', PositionIndicatorControlPanel );
-
-inherit( Panel, PositionIndicatorControlPanel );
 export default PositionIndicatorControlPanel;
