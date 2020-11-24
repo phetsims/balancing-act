cd029513960c152b5ef3adc89ce4a167e870ed45
jbphet <john.blanco@colorado.edu>
2020-11-23 17:10:52 -0700

    changed disabled button color back to null, see https://github.com/phetsims/balancing-act/issues/123

--- js/common/view/MassCarousel.js
+++ js/common/view/MassCarousel.js
@@ -14,7 +14,6 @@ import HBox from '../../../../scenery/js/nodes/HBox.js';
 import Node from '../../../../scenery/js/nodes/Node.js';
 import Text from '../../../../scenery/js/nodes/Text.js';
 import VBox from '../../../../scenery/js/nodes/VBox.js';
-import Color from '../../../../scenery/js/util/Color.js';
 import Carousel from '../../../../sun/js/Carousel.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
 import BoyCreatorNode from '../../balancelab/view/BoyCreatorNode.js';
@@ -45,7 +44,7 @@ class MassCarousel extends Carousel {
 
       // lightweight look for the buttons since the user must drag items across the buttons
       buttonColor: null,
-      buttonDisabledColor: Color.TRANSPARENT,
+      buttonDisabledColor: null,
       buttonStroke: null,
 
       tandem: Tandem.REQUIRED
