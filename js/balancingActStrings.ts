084f86e4b2554e89c733e5064144336330e19012
Sam Reid <reids@colorado.edu>
2021-11-18 17:44:01 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/balancingActStrings.ts
@@ -0,0 +1,51 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import balancingAct from './balancingAct.js';
+
+type StringsType = {
+  'balanceLab': string,
+  'level': string,
+  'people': string,
+  'forcesFromObjects': string,
+  'meters': string,
+  'kg': string,
+  'mysteryObjects': string,
+  'whatWillHappen': string,
+  'massLabelH': string,
+  'massLabelG': string,
+  'massLabelF': string,
+  'rulers': string,
+  'massLabelE': string,
+  'massLabelD': string,
+  'massLabels': string,
+  'unknownMassLabel': string,
+  'massLabelC': string,
+  'massLabelB': string,
+  'massLabelA': string,
+  'whatIsTheMass': string,
+  'bricks': string,
+  'pattern0Label1Value': string,
+  'pattern0Value1Units': string,
+  'intro': string,
+  'balancing-act': {
+    'title': string
+  },
+  'show': string,
+  'game': string,
+  'balanceMe': string,
+  'position': string,
+  'none': string,
+  'marks': string,
+  'startOver': string
+};
+
+const balancingActStrings = getStringModule( 'BALANCING_ACT' ) as StringsType;
+
+balancingAct.register( 'balancingActStrings', balancingActStrings );
+
+export default balancingActStrings;
