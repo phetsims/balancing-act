f9776080f3e7cc6522ff67c365460d50366bf82c
phet-dev <phetadmin@gmail.com>
2022-01-12 03:18:14 -0700

    update copyright dates from daily grunt work

--- js/common/view/BasicBalanceScreenView.js
+++ js/common/view/BasicBalanceScreenView.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2021, University of Colorado Boulder
+// Copyright 2013-2022, University of Colorado Boulder
 
 /**
  * ScreenView that displays items in the balance model, primarily the balance beam (i.e. the plank), the various masses,
