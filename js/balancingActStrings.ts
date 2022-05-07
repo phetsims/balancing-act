5013778e6a163d25a99f48820c080ab314bbe044
Sam Reid <reids@colorado.edu>
2022-05-07 09:35:40 -0600

    Use semicolons to terminate fields in type StringType, see https://github.com/phetsims/chipper/issues/1208

--- js/balancingActStrings.ts
+++ js/balancingActStrings.ts
@@ -8,40 +8,40 @@ import getStringModule from '../../chipper/js/getStringModule.js';
 import balancingAct from './balancingAct.js';
 
 type StringsType = {
-  'balanceLab': string,
-  'level': string,
-  'people': string,
-  'forcesFromObjects': string,
-  'meters': string,
-  'kg': string,
-  'mysteryObjects': string,
-  'whatWillHappen': string,
-  'massLabelH': string,
-  'massLabelG': string,
-  'massLabelF': string,
-  'rulers': string,
-  'massLabelE': string,
-  'massLabelD': string,
-  'massLabels': string,
-  'unknownMassLabel': string,
-  'massLabelC': string,
-  'massLabelB': string,
-  'massLabelA': string,
-  'whatIsTheMass': string,
-  'bricks': string,
-  'pattern0Label1Value': string,
-  'pattern0Value1Units': string,
-  'intro': string,
+  'balanceLab': string;
+  'level': string;
+  'people': string;
+  'forcesFromObjects': string;
+  'meters': string;
+  'kg': string;
+  'mysteryObjects': string;
+  'whatWillHappen': string;
+  'massLabelH': string;
+  'massLabelG': string;
+  'massLabelF': string;
+  'rulers': string;
+  'massLabelE': string;
+  'massLabelD': string;
+  'massLabels': string;
+  'unknownMassLabel': string;
+  'massLabelC': string;
+  'massLabelB': string;
+  'massLabelA': string;
+  'whatIsTheMass': string;
+  'bricks': string;
+  'pattern0Label1Value': string;
+  'pattern0Value1Units': string;
+  'intro': string;
   'balancing-act': {
-    'title': string
-  },
-  'show': string,
-  'game': string,
-  'balanceMe': string,
-  'position': string,
-  'none': string,
-  'marks': string,
-  'startOver': string
+    'title': string;
+  };
+  'show': string;
+  'game': string;
+  'balanceMe': string;
+  'position': string;
+  'none': string;
+  'marks': string;
+  'startOver': string;
 };
 
 const balancingActStrings = getStringModule( 'BALANCING_ACT' ) as StringsType;
