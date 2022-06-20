334258060c828b89e232e6227ffed7bab2f0e71d
jbphet <john.blanco@colorado.edu>
2022-06-20 12:40:59 -0600

    changed the way score displays are provided to status bars and level selection buttons to be more type safe, see https://github.com/phetsims/vegas/issues/102

--- js/game/view/StartGameLevelNode.js
+++ js/game/view/StartGameLevelNode.js
@@ -12,9 +12,9 @@ import merge from '../../../../phet-core/js/merge.js';
 import ResetAllButton from '../../../../scenery-phet/js/buttons/ResetAllButton.js';
 import TimerToggleButton from '../../../../scenery-phet/js/buttons/TimerToggleButton.js';
 import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
-import { Node } from '../../../../scenery/js/imports.js';
-import { Text } from '../../../../scenery/js/imports.js';
+import { Node, Text } from '../../../../scenery/js/imports.js';
 import LevelSelectionButton from '../../../../vegas/js/LevelSelectionButton.js';
+import ScoreDisplayStars from '../../../../vegas/js/ScoreDisplayStars.js';
 import vegasStrings from '../../../../vegas/js/vegasStrings.js';
 import balancingAct from '../../balancingAct.js';
 import BASharedConstants from '../../common/BASharedConstants.js';
@@ -75,10 +75,10 @@ class StartGameLevelNode extends Node {
         {
           listener: createLevelStartFunction( i ),
           baseColor: options.buttonBackgroundColor,
-          scoreDisplayOptions: {
+          createScoreDisplay: scoreProperty => new ScoreDisplayStars( scoreProperty, {
             numberOfStars: options.numStarsOnButtons,
             perfectScore: options.perfectScore
-          },
+          } ),
           soundPlayerIndex: i
         }
       );
