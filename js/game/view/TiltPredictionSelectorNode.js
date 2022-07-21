faec00c1f55fc67dc4f83b4b507ddf8eaab80eb3
phet-dev <phetadmin@gmail.com>
2022-07-21 04:03:14 -0600

    update copyright dates from daily grunt work

--- js/game/view/TiltPredictionSelectorNode.js
+++ js/game/view/TiltPredictionSelectorNode.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2021, University of Colorado Boulder
+// Copyright 2014-2022, University of Colorado Boulder
 
 /**
  * A Scenery node that defines a user interface element which allows the user
