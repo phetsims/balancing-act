eb237c0188f6606fe0b7d81f52b20dc38060d5f9
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:39 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/balancingActStrings.ts
+++ js/balancingActStrings.ts
@@ -5,43 +5,76 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import balancingAct from './balancingAct.js';
 
 type StringsType = {
   'balanceLab': string;
+  'balanceLabProperty': TReadOnlyProperty<string>;
   'level': string;
+  'levelProperty': TReadOnlyProperty<string>;
   'people': string;
+  'peopleProperty': TReadOnlyProperty<string>;
   'forcesFromObjects': string;
+  'forcesFromObjectsProperty': TReadOnlyProperty<string>;
   'meters': string;
+  'metersProperty': TReadOnlyProperty<string>;
   'kg': string;
+  'kgProperty': TReadOnlyProperty<string>;
   'mysteryObjects': string;
+  'mysteryObjectsProperty': TReadOnlyProperty<string>;
   'whatWillHappen': string;
+  'whatWillHappenProperty': TReadOnlyProperty<string>;
   'massLabelH': string;
+  'massLabelHProperty': TReadOnlyProperty<string>;
   'massLabelG': string;
+  'massLabelGProperty': TReadOnlyProperty<string>;
   'massLabelF': string;
+  'massLabelFProperty': TReadOnlyProperty<string>;
   'rulers': string;
+  'rulersProperty': TReadOnlyProperty<string>;
   'massLabelE': string;
+  'massLabelEProperty': TReadOnlyProperty<string>;
   'massLabelD': string;
+  'massLabelDProperty': TReadOnlyProperty<string>;
   'massLabels': string;
+  'massLabelsProperty': TReadOnlyProperty<string>;
   'unknownMassLabel': string;
+  'unknownMassLabelProperty': TReadOnlyProperty<string>;
   'massLabelC': string;
+  'massLabelCProperty': TReadOnlyProperty<string>;
   'massLabelB': string;
+  'massLabelBProperty': TReadOnlyProperty<string>;
   'massLabelA': string;
+  'massLabelAProperty': TReadOnlyProperty<string>;
   'whatIsTheMass': string;
+  'whatIsTheMassProperty': TReadOnlyProperty<string>;
   'bricks': string;
+  'bricksProperty': TReadOnlyProperty<string>;
   'pattern0Label1Value': string;
+  'pattern0Label1ValueProperty': TReadOnlyProperty<string>;
   'pattern0Value1Units': string;
+  'pattern0Value1UnitsProperty': TReadOnlyProperty<string>;
   'intro': string;
+  'introProperty': TReadOnlyProperty<string>;
   'balancing-act': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'show': string;
+  'showProperty': TReadOnlyProperty<string>;
   'game': string;
+  'gameProperty': TReadOnlyProperty<string>;
   'balanceMe': string;
+  'balanceMeProperty': TReadOnlyProperty<string>;
   'position': string;
+  'positionProperty': TReadOnlyProperty<string>;
   'none': string;
+  'noneProperty': TReadOnlyProperty<string>;
   'marks': string;
+  'marksProperty': TReadOnlyProperty<string>;
   'startOver': string;
+  'startOverProperty': TReadOnlyProperty<string>;
 };
 
 const balancingActStrings = getStringModule( 'BALANCING_ACT' ) as StringsType;
