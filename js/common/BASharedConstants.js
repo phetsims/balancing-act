19a14ab881ee973eacbf9f895aa0c6036c33e65a
AgustinVallejo <agustin.vallejo@udea.edu.co>
2023-12-05 14:14:27 -0500

    Removing layoutBounds from BASharedConstants.js, see https://github.com/phetsims/balancing-act/issues/109

--- js/common/BASharedConstants.js
+++ js/common/BASharedConstants.js
@@ -1,12 +1,10 @@
 // Copyright 2014-2021, University of Colorado Boulder
 
 
-import Bounds2 from '../../../dot/js/Bounds2.js';
 import balancingAct from '../balancingAct.js';
 
 const BASharedConstants = {
   COMPARISON_TOLERANCE: 1E-6,
-  LAYOUT_BOUNDS: new Bounds2( 0, 0, 768, 504 ),
   RESET_ALL_BUTTON_RADIUS: 20
 };
 
