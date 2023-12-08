9006f7844b386846816455d64ff6764c18b95919
Marla Schulz <marlaa.schulz@gmail.com>
2023-12-08 14:23:29 -0700

    Document custom layout bounds across multiple screen views to prevent future breaks in phet-io instrumentation, https://github.com/phetsims/phet-io/issues/1939.

--- js/common/BASharedConstants.js
+++ js/common/BASharedConstants.js
@@ -6,6 +6,10 @@ import balancingAct from '../balancingAct.js';
 
 const BASharedConstants = {
   COMPARISON_TOLERANCE: 1E-6,
+
+  // A PhET wide decision was made to not update custom layout bounds even if they do not match the
+  // default layout bounds in ScreenView. Do not change these bounds as changes could break or disturb
+  // any phet-io instrumention. https://github.com/phetsims/phet-io/issues/1939
   LAYOUT_BOUNDS: new Bounds2( 0, 0, 768, 504 ),
   RESET_ALL_BUTTON_SCALE: 0.96
 };
