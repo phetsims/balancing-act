e22d7bc617a3e6851b7ecc629c991384e70c2439
Luisav1 <luisa.vargassuarez@ucalgary.ca>
2023-12-11 10:04:56 -0700

    Add doc. See https://github.com/phetsims/balancing-act/issues/92.

--- js/game/view/MassValueEntryNode.js
+++ js/game/view/MassValueEntryNode.js
@@ -40,7 +40,7 @@ class MassValueEntryNode extends Node {
     // Create and add the readout, including the background.
     const readoutText = new Text( StringUtils.format( pattern0Value1UnitsString, 0, kgString ), {
       font: READOUT_FONT,
-      maxWidth: 200 // empirically determined
+      maxWidth: 200 // empirically determined based on tests with long strings
     } );
     const readoutBackground = new Rectangle( 0, 0, readoutText.width * 2.5, readoutText.height * 1.3, 4, 4,
       {
