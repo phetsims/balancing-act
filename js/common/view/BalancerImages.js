463175e052cc68363ed376011fd48543ba43474e
phet-dev <phetadmin@gmail.com>
2024-01-13 03:10:23 -0700

    update copyright dates from daily grunt work

--- js/common/view/BalancerImages.js
+++ js/common/view/BalancerImages.js
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2023-2024, University of Colorado Boulder
 
 /**
  * Responsible for all images for the Balancer in this sim. Collects the images to support selecting a different
