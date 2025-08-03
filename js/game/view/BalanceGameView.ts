51e0d69f97f45756feb8bed53d8e77766beb1d91
Sam Reid <reids@colorado.edu>
2025-08-03 17:43:59 -0600

    Revert to levelProperty, see https://github.com/phetsims/balancing-act/issues/168

--- js/game/view/BalanceGameView.ts
+++ js/game/view/BalanceGameView.ts
@@ -240,7 +240,9 @@ export default class BalanceGameView extends ScreenView {
         numberOfChallengesProperty: new Property( BalanceGameModel.PROBLEMS_PER_LEVEL ),
 
         // FiniteStatusBar uses 1-based level numbering, model is 0-based, see #85.
-        levelNumberProperty: new DerivedProperty( [ gameModel.levelProperty ], ( level: number ) => level + 1 ),
+        // @ts-expect-error - see https://github.com/phetsims/balancing-act/issues/168
+        levelProperty: new DerivedProperty( [ gameModel.levelProperty ], ( level: number ) => level + 1 ),
+
         elapsedTimeProperty: gameModel.elapsedTimeProperty,
         timerEnabledProperty: gameModel.timerEnabledProperty,
         startOverButtonText: startOverStringProperty,
