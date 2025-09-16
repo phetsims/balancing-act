29c9ef90bc22aff41afdfcc271bd3fc780fd08d4
Sam Reid <reids@colorado.edu>
2025-09-16 15:57:28 -0600

    Fix author annotation

--- js/common/model/PositionedVector.ts
+++ js/common/model/PositionedVector.ts
@@ -4,7 +4,7 @@
  * Type definition for a vector that has both an origin point and a directional vector component.
  * This is used throughout the Balancing Act simulation for force vectors and positioned vectors.
  *
- * @author Claude AI Assistant
+ * @author Sam Reid (PhET Interactive Simulations)
  */
 
 import Vector2 from '../../../../dot/js/Vector2.js';
